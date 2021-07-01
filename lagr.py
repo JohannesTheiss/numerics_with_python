bbdfae2088cd20c5e01ee8b5a6b086c8a835753b
JohannesTheiss <johannestheiss00@gmail.com>
2021-07-01 23:33:27 +0200

    add some spline stuff

--- lagr.py
+++ lagr.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.interpolate import lagrange
+from scipy.interpolate import lagrange, interp1d
 from numpy.polynomial.polynomial import Polynomial
 import matplotlib.pyplot as plt
 import math
@@ -143,52 +143,100 @@ def newton(xi, fi, x, debug=True):
     print(f"P 0,{n-1} = p({x}) = {p[0]}")
     return func, p[0]
 
+def spline_inter(xi, fi):
+    sp = interp1d(xi, fi, kind='cubic')
+    return sp
+
+
+
+
+
+################## SETTINGS ################### 
+LAGRAGE = False
+NEVILLE = False
+NEWTON = False
+SPLINE = True
+
 
 ################### INPUTS ################### 
 # Stuetzstellen
-xi = np.array([-2, -1, 1, 3])
+#xi = np.array([-2, -1, 1, 3])
 #xi = np.array([-2, -0.5, 0.5, 1, 1.5])
+xi = np.array([-1, 0, 1, 2])
 
 # Stuetzwerte
-fi = np.array([8, 0, 2, -12])
+#fi = np.array([8, 0, 2, -12])
 #fi = np.array([-4, 0.5, 3.5, 5, 6.5])
+f = lambda x : abs(x)
+fi = np.array(f(xi))
 
 # Auswertungspunkte
 #X = np.arange(-10, 10)
 X = np.array([2])
 x = X[0]
 
+# functions to plot
+funcs = [PlotFunc(xi, fi, LINE_TYPE.line, color="r", name="input_func")]
 
-################### Interpolation ################### 
-# lagrange call
-lagr_coef, lagr_values = lagr(xi, fi, X)
-
-# plot the lagr poly as a function
 xi_max = math.ceil(xi.max())
 xi_min = math.ceil(xi.min())
-lagr_X = np.linspace(xi_min, xi_max)
-lagr_Y = np.polyval(lagr_coef, lagr_X)
+new_X = np.linspace(xi_min, xi_max)
+
+################### Interpolation ################### 
+if LAGRAGE:
+    # lagrange call
+    lagr_coef, lagr_values = lagr(xi, fi, X)
+
+    # plot the lagr poly as a function
+    lagr_Y = np.polyval(lagr_coef, new_X)
+
+    if lagr_Y.size != 0:
+        funcs.append(PlotFunc(new_X, lagr_Y, name="lagrange_poly"))
+    if lagr_values != None:
+        funcs.append(PlotFunc(X, lagr_values, name="lagrange_values"))
 
 # NEVILLE call
-neville_value = None
-neville_value = neville(xi, fi, x)
+if NEVILLE:
+    neville_value = None
+    neville_value = neville(xi, fi, x)
+
+    if neville_value != None:
+        funcs.append(PlotFunc(x, neville_value, name="neville_value"))
 
 # NEWTON call
-newton_value = None
-horner_schema, newton_value = newton(xi, fi, x)
+if NEWTON:
+    newton_value = None
+    horner_schema, newton_value = newton(xi, fi, x)
+    newton_y = horner_schema(new_X)
+
+    if horner_schema != None:
+        funcs.append(PlotFunc(new_X, newton_y, name="newton_poly"))
+    if newton_value != None:
+        funcs.append(PlotFunc(x, newton_value, name="newton_value"))
+
+if SPLINE:
+    spline = spline_inter(xi, fi)
+
+    spline_values = np.array(spline(new_X))
+
+    if spline_values.size != 0:
+        funcs.append(PlotFunc(new_X, spline_values, name="spline_value"))
+
+
+
+############ ERROR ##################
+#error = False
+#er = []
+#for i in range(len(newton_y)):
+#    er.append(np.round(abs(newton_y[i] - lagr_Y[i]), 9))
+#    if er[i] > 0.0001:
+#        error = True
+
+#print(error)
+#print(er)
 
 
 ################### PLOTTING ################### 
-funcs = [PlotFunc(xi, fi, LINE_TYPE.line, color="r", name="input_func")]
-
-if lagr_Y.size != 0:
-    funcs.append(PlotFunc(lagr_X, lagr_Y, name="lagrange_poly"))
-if lagr_values != None:
-    funcs.append(PlotFunc(X, lagr_values, name="lagrange_values"))
-if newton_value != None:
-    funcs.append(PlotFunc(x, newton_value, name="newton_value"))
-if neville_value != None:
-    funcs.append(PlotFunc(x, neville_value, name="neville_value"))
 
 
 plot_funcs(funcs)
